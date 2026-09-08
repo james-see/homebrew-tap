b5fd5a6879f88aa45f77abb1f672219bf4e05cae
James Campbell <james@jamescampbell.us>
2026-09-08 11:41:05 -0400

    Bump temper formula to 0.1.8.
    Hermes sidecar: temper hermes opens their TUI; Temper supervises and can inject recovery.

--- Formula/temper.rb
+++ Formula/temper.rb
@@ -4,21 +4,21 @@
 class Temper < Formula
   desc "Adaptive control plane for coding agents"
   homepage "https://temper.baby"
-  version "0.1.7"
+  version "0.1.8"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/james-see/temper/releases/download/v0.1.7/temper_0.1.7_darwin_amd64.tar.gz"
-      sha256 "7ef807e0cf8a88c5df9f79d8dab3b8e576e9f9bab37c13130352aa91136a84ea"
+      url "https://github.com/james-see/temper/releases/download/v0.1.8/temper_0.1.8_darwin_amd64.tar.gz"
+      sha256 "5e4e2cf2b044deff54b232402b864f76fe2addcfb8877840c9c097008aa98aca"
 
       def install
         bin.install "temper"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/james-see/temper/releases/download/v0.1.7/temper_0.1.7_darwin_arm64.tar.gz"
-      sha256 "407dafac9de6755db2868ed6159c91cd062c5d2bfba4b08d6cf6da4de56e7abe"
+      url "https://github.com/james-see/temper/releases/download/v0.1.8/temper_0.1.8_darwin_arm64.tar.gz"
+      sha256 "b30b00a5fb3ef6fc0fc64827426ac8464a9ac12e0555da23f92561dafd7760dc"
 
       def install
         bin.install "temper"
@@ -28,15 +28,15 @@ class Temper < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/james-see/temper/releases/download/v0.1.7/temper_0.1.7_linux_amd64.tar.gz"
-      sha256 "9cf67bf6ac20b770d5bfb3d3b8fa887e0d97c339072ee43a26e4e51cc0bd6908"
+      url "https://github.com/james-see/temper/releases/download/v0.1.8/temper_0.1.8_linux_amd64.tar.gz"
+      sha256 "476e2b09d1eb2c4d2c624851bc775153ae86c95ea6870899be50d19d409e1889"
       def install
         bin.install "temper"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/james-see/temper/releases/download/v0.1.7/temper_0.1.7_linux_arm64.tar.gz"
-      sha256 "585ea36710d34e191c2d8d1df551ea9cc327ead9042f435f600ae5667035eb48"
+      url "https://github.com/james-see/temper/releases/download/v0.1.8/temper_0.1.8_linux_arm64.tar.gz"
+      sha256 "cd02cad5f3f945d9279d9efc17a9fbe4806fe6e810783a48ed2f00758d912816"
       def install
         bin.install "temper"
       end
@@ -44,6 +44,6 @@ class Temper < Formula
   end
 
   test do
-    assert_match "temper 0.1.7", shell_output("#{bin}/temper version")
+    assert_match "temper 0.1.8", shell_output("#{bin}/temper version")
   end
 end
